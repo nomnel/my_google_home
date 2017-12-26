67fe26e2a2074c8393c43cef3d0e591b0f1fb544
nomnel <laxyz.nomnel@gmail.com>
2017-12-26 10:48:06 +0900

    GET パラメータで文言を指定できる

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
+const express = require("express");
+const app = express();
+
 const googlehome = require('google-home-notifier')
 const language = 'ja';
-
 googlehome.device('Google-Home', language);
 
-googlehome.notify('こんにちは。私はグーグルホームです。', function(res) {
-  console.log(res);
-});;
+const server = app.listen(8000, function() {
+  console.log("Node.js is listening to PORT:" + server.address().port);
+});
+
+app.get("/messages", function(req, res, next) {
+  const body = req.query.body || 'こんにちは。私はグーグルホームです。';
+  googlehome.notify(body, function(res) {
+    console.log(res);
+  });
+  res.json(null);
+});
